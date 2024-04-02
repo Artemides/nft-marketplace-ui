1c0601feadf7463ecab83e2f3e6851b7e57956c9
Artemides <edmpulasky@gmail.com>
2024-04-01 22:26:59 -0500

    migrate field input traits array

--- src/components/AstroUploadF.tsx
+++ src/components/AstroUploadF.tsx
@@ -1,6 +1,6 @@
 import { NFT } from "@/types/types";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
@@ -11,20 +11,32 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import Trait from "./Trait2";
+import { newTrait } from "@/constants/constants";
+import { Button } from "./ui/button";
 
 const AstroUploadF = () => {
   const form = useForm<NFT>();
+  const {
+    append,
+    fields: traits,
+    remove,
+  } = useFieldArray({
+    control: form.control,
+    name: "traits",
+  });
+
   return (
     <section id="upload-nft" className="grid grid-cols-2 gap-x-12 pl-8">
       <div className="min-h-fit pt-12">
         <Form {...form}>
-          <form>
+          <form className="flex flex-col gap-y-4">
             <FormField
               control={form.control}
               name="name"
               render={({ field, fieldState: { error } }) => (
                 <FormItem>
-                  <FormLabel>Name: *</FormLabel>
+                  <FormLabel className="font-semibold">Name: *</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
@@ -41,7 +53,9 @@ const AstroUploadF = () => {
               name="description"
               render={({ field, fieldState: { error } }) => (
                 <FormItem>
-                  <FormLabel>Name: *</FormLabel>
+                  <FormLabel className="font-semibold">
+                    Description: *
+                  </FormLabel>
                   <FormControl>
                     <Textarea
                       {...field}
@@ -54,6 +68,35 @@ const AstroUploadF = () => {
                 </FormItem>
               )}
             />
+            <div>
+              <p className="font-semibold">Traits</p>
+              <span className="text-neutral-300">
+                Traits describe attributes of your item. They appear as filters
+                inside your collection page and are also listed out inside your
+                item page.
+              </span>
+              <div className="my-2 space-y-2">
+                {traits.map((trait, idx) => (
+                  <Trait
+                    key={trait.id}
+                    control={form.control}
+                    idx={idx}
+                    onDelete={remove}
+                  />
+                ))}
+                <Button
+                  variant={"ghost"}
+                  size={"sm"}
+                  type="button"
+                  className="font-semibold "
+                  onClick={() => {
+                    append(newTrait);
+                  }}
+                >
+                  + Add trait
+                </Button>
+              </div>
+            </div>
           </form>
         </Form>
       </div>
---
+++ src/components/Trait2.tsx
@@ -0,0 +1,78 @@
+import { IoMdClose } from "react-icons/io";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Control } from "react-hook-form";
+import { NFT } from "@/types/types";
+import { Input } from "./ui/input";
+
+type TraitProps = {
+  idx: number;
+  onDelete: (idx: number) => void;
+  disabled?: boolean;
+  control: Control<NFT, any>;
+};
+
+const Trait: React.FC<TraitProps> = ({ idx, onDelete, disabled, control }) => {
+  const handleDeleteTrait = () => {
+    onDelete(idx);
+  };
+
+  return (
+    <div className="flex justify-between items-center  p-3 bg-neutral-900/50 rounded-lg">
+      <div className="flex-1 border-r border-r-neutral-500/50 pr-3">
+        <div className="flex gap-x-2 items-top">
+          <FormField
+            control={control}
+            name={`traits.${idx}.type`}
+            render={({ field, fieldState: { error } }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="Trait"
+                    className={error && "ring-1 ring-red-500"}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name={`traits.${idx}.value`}
+            render={({ field, fieldState: { error } }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="Value"
+                    className={error && "ring-1 ring-red-500"}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </div>
+      <div className=" flex items-center pl-3 space-x-4">
+        <button
+          type="button"
+          onClick={handleDeleteTrait}
+          className="hover:text-neutral-400 disabled:cursor-not-allowed disabled:text-neutral-800"
+          disabled={disabled}
+        >
+          <IoMdClose size={21} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Trait;
