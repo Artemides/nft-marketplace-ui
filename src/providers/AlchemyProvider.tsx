f0eecdd9da45e632d08fdfb66d7259a0b1db8006
Artemides <edmpulasky@gmail.com>
2024-04-15 16:43:47 -0500

    wrap app within alchemy react provider and config wagmi with alchemy rpc url

--- src/providers/AlchemyProvider.tsx
+++ src/providers/AlchemyProvider.tsx
@@ -1,7 +1,13 @@
-import { createContext } from "vm";
+import { Alchemy, AlchemySettings, Network } from "alchemy-sdk";
+import { config } from "../../config";
+import { ReactNode, createContext } from "react";
+export const AlchemyContext = createContext<Alchemy | undefined>(undefined);
 
-export const AlchemyContext = createContext();
-export const AlchemyProvider = () => {
-    const {}=new 
-    return 
+const settings: AlchemySettings = {
+  apiKey: config.alchemyApiKey,
+  network: Network.ETH_SEPOLIA,
+};
+export const AlchemyProvider = ({ children }: { children: ReactNode }) => {
+  const alchemy = new Alchemy(settings);
+  return <AlchemyContext.Provider value={alchemy}>{children}</AlchemyContext.Provider>;
 };
